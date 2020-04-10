f61d48e099b3dfd3087fcc6a7f257a7e19dbcd9f
lzy565418918 <565418918@qq.com>
2020-04-10 10:16:35 +0800

    拉取配置文件

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// -----------按需加载----------
+import Button from 'antd/es/button'; // 加载 JS
+import 'antd/es/button/style/css'; // 加载 CSS
+import Steps from 'antd/es/steps'; // 加载 JS
+import 'antd/es/steps/style/css'; // 加载 CSS
+// ------------------------------
+// import { Button, Steps } from 'antd';
+const { Step } = Steps
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+      <Button type="dashed" danger>Dashed</Button>
+      <Button type="link">Link</Button>
+      <Steps current={1}>
+    <Step title="Finished" description="This is a description." />
+    <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
+    <Step title="Waiting" description="This is a description." />
+  </Steps>,
     </div>
   );
 }
